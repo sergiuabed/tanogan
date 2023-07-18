9b9cde5387b1a110f44d5245bb8932d6c8989e3f
Sergiu Abed s295149 <s295149@studenti.polito.it>
2023-07-18 17:56:41 +0200

    changes in generator and discriminator initializer functions

--- architecture.py
+++ architecture.py
@@ -2,25 +2,33 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
-def init_generator(window_dim, in_dim, out_dim):
+def init_generator(in_dim, out_dim):
+#def init_generator(window_dim, in_dim, out_dim):
+    nr_units_dense = 1
+    for i in out_dim:
+        nr_units_dense *= i
+
+    
     generator = keras.Sequential(
         [
-            keras.Input(shape=(window_dim, in_dim)),
+            keras.Input(shape=in_dim),
             layers.LSTM(32, return_sequences=True),
             layers.LSTM(64, return_sequences=True),
             layers.LSTM(128),
-            layers.Dense(out_dim, activation="tanh")
+            layers.Dense(nr_units_dense, activation="tanh"),
+            layers.Reshape(out_dim)
         ]
     )
 
     return generator
 
-def init_discriminator(window_dim, in_dim, out_dim):
+def init_discriminator(in_dim, out_dim):
+#def init_discriminator(window_dim, in_dim, out_dim):
     discriminator = keras.Sequential(
         [
-            keras.Input(shape=(window_dim, in_dim)),
+            keras.Input(shape=in_dim),
             layers.LSTM(100),
-            layers.Dense(out_dim, activation="sigmoid")   # out_dim should be 1
+            layers.Dense(out_dim, activation="sigmoid")   # out_dim should be (1,)
         ]
     )
 
