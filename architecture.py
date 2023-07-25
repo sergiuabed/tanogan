3f8c400d64e24997188c5297ddcc9a08849e1726
Sergiu Abed s295149 <s295149@studenti.polito.it>
2023-07-25 16:58:23 +0200

    added encoder architecture and removed the flatten layers

--- architecture.py
+++ architecture.py
@@ -15,7 +15,6 @@ def init_generator(in_dim, out_dim):
             layers.LSTM(32, return_sequences=True),
             layers.LSTM(64, return_sequences=True),
             layers.LSTM(128),
-            layers.Flatten(),
             layers.Dense(nr_units_dense, activation="tanh"),
             layers.Reshape(out_dim)
         ]
@@ -29,9 +28,26 @@ def init_discriminator(in_dim, out_dim):
         [
             keras.Input(shape=in_dim),
             layers.LSTM(100),
-            layers.Flatten(),
             layers.Dense(out_dim, activation="sigmoid")   # out_dim should be (1,)
         ]
     )
 
     return discriminator
+
+def init_encoder(in_dim, out_dim):
+    nr_units_dense = 1
+    for i in out_dim:
+        nr_units_dense *= i
+
+    encoder = keras.Sequential(
+        [
+            keras.Input(shape=in_dim),
+            layers.LSTM(128, return_sequences=True),
+            layers.LSTM(64, return_sequences=True),
+            layers.LSTM(32),
+            layers.Dense(nr_units_dense, activation="tanh"),
+            layers.Reshape(out_dim)
+        ]
+    )
+
+    return encoder
